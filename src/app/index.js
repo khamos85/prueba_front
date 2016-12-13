98c20187597422dd27944a0fe37ef4a361201578
dp17 <diegoparra@tiendosqui.com>
2016-12-13 08:37:37 -0500

    Initial Commit

---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    angular.module('singApp', [
+        'singApp.core',
+        'singApp.dashboard',
+        'singApp.another',
+        'singApp.profile',
+        'singApp.login',
+        'singApp.error'
+    ]);
+})();
